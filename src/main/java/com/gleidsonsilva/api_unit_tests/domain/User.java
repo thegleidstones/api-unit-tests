3fd701c3fe4629f0520a6d1ce477fcf4c091ee13
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-26 23:09:26 -0300

    User class

---
+++ src/main/java/com/gleidsonsilva/api_unit_tests/domain/User.java
@@ -0,0 +1,26 @@
+package com.gleidsonsilva.api_unit_tests.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
