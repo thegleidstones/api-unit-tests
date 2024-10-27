12c75980baaeaf349111fa7f85cca3cf380b3385
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-27 00:09:39 -0300

    class StandardError for create a basic error to client.

---
+++ src/main/java/com/gleidsonsilva/api_unit_tests/resources/exceptions/StandardError.java
@@ -0,0 +1,19 @@
+package com.gleidsonsilva.api_unit_tests.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StandardError {
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+}
