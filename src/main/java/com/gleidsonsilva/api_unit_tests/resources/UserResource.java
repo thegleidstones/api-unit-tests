d3662cbb6ee90928c339249195e4a38f0afc959a
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-27 01:00:20 -0300

    create end point to create a new user.

--- src/main/java/com/gleidsonsilva/api_unit_tests/resources/UserResource.java
+++ src/main/java/com/gleidsonsilva/api_unit_tests/resources/UserResource.java
@@ -1,15 +1,15 @@
 package com.gleidsonsilva.api_unit_tests.resources;
 
+import com.gleidsonsilva.api_unit_tests.domain.User;
 import com.gleidsonsilva.api_unit_tests.domain.dto.UserDTO;
 import com.gleidsonsilva.api_unit_tests.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,6 +31,13 @@ public class UserResource {
     public ResponseEntity<List<UserDTO>> findAll() {
         return ResponseEntity.ok(service.findAll()
                 .stream().map(user -> mapper.map(user, UserDTO.class)).toList());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO dto) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(service.create(dto).getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
