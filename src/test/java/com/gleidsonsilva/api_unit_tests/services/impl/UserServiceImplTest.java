160eed9c14e78d59fb5e9ae7f25c44d4dfd0fb4d
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-27 20:27:26 -0300

    - create test for User not found; - create tests for method findAll();

--- src/test/java/com/gleidsonsilva/api_unit_tests/services/impl/UserServiceImplTest.java
+++ src/test/java/com/gleidsonsilva/api_unit_tests/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.gleidsonsilva.api_unit_tests.services.impl;
 import com.gleidsonsilva.api_unit_tests.domain.User;
 import com.gleidsonsilva.api_unit_tests.domain.dto.UserDTO;
 import com.gleidsonsilva.api_unit_tests.repositories.UserRepository;
+import com.gleidsonsilva.api_unit_tests.services.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,6 +12,7 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,6 +26,8 @@ class UserServiceImplTest {
     public static final String NAME = "Gleidson Silva";
     public static final String EMAIL = "gleidson@gmail.com";
     public static final String PASSWORD = "123";
+    public static final String USUARIO_NAO_ENCONTRADO = "Usuário não encontrado";
+    public static final Integer INDEX = 0;
     @InjectMocks
     private UserServiceImpl service;
 
@@ -58,7 +62,30 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindByIdThenReturnAnUserNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new UserNotFoundException(USUARIO_NAO_ENCONTRADO));
+        try {
+            service.findById(ID);
+        } catch (Exception exception) {
+            assertEquals(UserNotFoundException.class, exception.getClass());
+            assertEquals(USUARIO_NAO_ENCONTRADO, exception.getMessage());
+        }
+    }
+
+    @Test
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
