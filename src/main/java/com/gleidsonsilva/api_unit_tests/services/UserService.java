4b4439c05015a1cb75553ccae32b54dca55a8c4e
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-27 00:41:28 -0300

    create method findAll

--- src/main/java/com/gleidsonsilva/api_unit_tests/services/UserService.java
+++ src/main/java/com/gleidsonsilva/api_unit_tests/services/UserService.java
@@ -3,7 +3,10 @@ package com.gleidsonsilva.api_unit_tests.services;
 import com.gleidsonsilva.api_unit_tests.domain.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
