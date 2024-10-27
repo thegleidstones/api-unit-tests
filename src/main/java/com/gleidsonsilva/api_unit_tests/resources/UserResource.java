c75076489439ac2a117decfe9faf74dc82f97bea
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-26 23:40:12 -0300

    class user resource findById

--- src/main/java/com/gleidsonsilva/api_unit_tests/resources/UserResource.java
+++ src/main/java/com/gleidsonsilva/api_unit_tests/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.gleidsonsilva.api_unit_tests.resources;
 
 import com.gleidsonsilva.api_unit_tests.domain.User;
+import com.gleidsonsilva.api_unit_tests.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok(new User(1, "Gleidson Silva", "gleidson.silva@gmail.com", "123456789"));
+        return ResponseEntity.ok(service.findById(id));
     }
 
 }
