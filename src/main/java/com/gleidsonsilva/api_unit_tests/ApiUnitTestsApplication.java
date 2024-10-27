4a9b62a32f18227d55e195bc4e7421d03dc85295
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-26 22:51:30 -0300

    initial commit.

---
+++ src/main/java/com/gleidsonsilva/api_unit_tests/ApiUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.gleidsonsilva.api_unit_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUnitTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gleidsonsilva/api_unit_tests/ApiUnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleidsonsilva.api_unit_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUnitTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
