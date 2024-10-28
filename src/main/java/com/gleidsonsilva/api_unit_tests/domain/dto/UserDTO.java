24bb3975e69a6ac5b37af61ea3cbff52e1961bfb
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-28 18:17:47 -0300

    Add validations to UserDTO attributes.

--- src/main/java/com/gleidsonsilva/api_unit_tests/domain/dto/UserDTO.java
+++ src/main/java/com/gleidsonsilva/api_unit_tests/domain/dto/UserDTO.java
@@ -1,6 +1,9 @@
 package com.gleidsonsilva.api_unit_tests.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,9 +15,16 @@ import lombok.Setter;
 @NoArgsConstructor
 public class UserDTO {
         private Integer id;
+
+        @NotBlank(message = "Nome não pode ser nulo ou vazio")
         private String name;
+
+        @NotBlank(message = "E-mail não pode ser nulo ou vazio")
+        @Email(message = "E-mail deve ser válido")
         private String email;
 
         @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+        @NotBlank(message = "Senha não pode ser nula ou vazia")
+        @Size(min = 6, max = 20, message = "Senha deve ter no mínimo 6 caracteres e máximo 20")
         private String password;
 }
