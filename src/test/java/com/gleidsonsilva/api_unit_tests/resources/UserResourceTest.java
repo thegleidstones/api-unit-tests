d62566d995b9f047405463868cfb7d9b2ba61de3
Gleidson Silva <silvamorais.ti@gmail.com>
2024-10-27 21:37:13 -0300

    create user resource test class;

---
+++ src/test/java/com/gleidsonsilva/api_unit_tests/resources/UserResourceTest.java
@@ -0,0 +1,68 @@
+package com.gleidsonsilva.api_unit_tests.resources;
+
+import com.gleidsonsilva.api_unit_tests.domain.User;
+import com.gleidsonsilva.api_unit_tests.domain.dto.UserDTO;
+import com.gleidsonsilva.api_unit_tests.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserResourceTest {
+    public static final Integer ID = 1;
+    public static final String NAME = "Gleidson Silva";
+    public static final String EMAIL = "gleidson@gmail.com";
+    public static final String PASSWORD = "123";
+    public static final Integer INDEX = 0;
+
+    private User user;
+    private UserDTO userDTO;
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
